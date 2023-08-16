991857083de2063e52b0be208b026d238a3ddb79
Zheng Li(MarsOriginal) <zlii0145@student.monash.edu>
2023-08-16 22:20:40 +1000

    Add typewriter effect

--- marsoriginal/src/pages/Home.js
+++ marsoriginal/src/pages/Home.js
@@ -1,21 +1,47 @@
 import React from 'react'
-import { LinkedIn, GitHub, Email } from '@material-ui/icons'
+import { Email } from '@material-ui/icons'
+import { Cursor, Typewriter } from 'react-simple-typewriter'
 import '../styles/Home.css'
 
 
 function Home() {
+
   return (
     <div className="home">
         <div className="about">
             <h2>Hello World!</h2>
             <div className="prompt">
-                <p>I'm a developer.</p>
-                <LinkedIn/>
-                <GitHub/>
+                <p>I'm a 
+                    <span> </span>
+                    <span style={{fontWeight: "bold", color: "green"}}>
+                        <Typewriter words={['Developer', 'Designer', 'Tempura Chef =)']}
+                            typeSpeed={100} loop={0}/>
+                    </span>
+                    <span>
+                        <Cursor cursorStyle='<'/>
+                    </span>
+                </p>
                 <Email/>
             </div>
         </div>
-        <div className="skills"></div>
+        <div className="skills">
+            <ol className="list">
+                <li className="item">
+                    <h2>FrontEnd</h2>
+                    <span>React, React Native, HTML, CSS, User-Centred Design</span>
+                </li>
+                <li className="item">
+                    <h2>BackEnd</h2>
+                    <span>NodeJS, ExpressJS, SQL, MongoDB</span>
+                </li>
+                <li className="item">
+                    <h2>Languages</h2>
+                    <span>Java, JavaScript, Python, C, Swift</span>
+                    <br/>
+                    <span>Of course, I also understand: English & Mandarin</span>
+                </li>
+            </ol>
+        </div>
     </div>
   )
 }
