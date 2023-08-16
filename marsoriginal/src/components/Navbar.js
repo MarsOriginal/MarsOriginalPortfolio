0cee3a34d95749d7abf7a2e854e437da60b07e66
Zheng Li(MarsOriginal) <zlii0145@student.monash.edu>
2023-08-16 17:05:15 +1000

    add a logo and fix a typo

--- marsoriginal/src/components/Navbar.js
+++ marsoriginal/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Link, useLocation } from "react-router-dom";
 import '../styles/Navbar.css';
+import logo from '../assets/mologo500.png'
 import Reorder from '@material-ui/icons/Reorder';
 
 function Navbar() {
@@ -12,10 +13,11 @@ function Navbar() {
         setExpand(false);
     }, [location]);
   return (
-    <div className="navbar" id={expand ? "open" : "closed"}>
+    <div className="navbar" id={expand ? "open" : "close"}>
         <div className="toggleButton">
             <button onClick={() => setExpand(((prev) => !prev))}> <Reorder/> </button>
         </div>
+        <img src={logo} alt="logo" className="logo"/>
         <div className="links">
             <Link to="/">Home</Link>
             <Link to="/projects">Projects</Link>
--- marsoriginal/src/pages/Home.js
+++ marsoriginal/src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { LinkedIn, GitHub, Email } from '@material-ui/icons'
+import '../styles/Home.css'
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+        <div className="about">
+            <h2>Hello World!</h2>
+            <div className="prompt">
+                <p>I'm a developer.</p>
+                <LinkedIn/>
+                <GitHub/>
+                <Email/>
+            </div>
+        </div>
+        <div className="skills"></div>
+    </div>
   )
 }
 
